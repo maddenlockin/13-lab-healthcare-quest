d2813b8b031319405a4c6476091abc20594c90ce
maddenlockin <maddenlockin@gmail.com>
2021-05-26 14:32:52 -0700

    ./

--- results/results.js
+++ results/results.js
@@ -18,5 +18,5 @@ resultsP.textContent = getEndingMessage(user);
 button.addEventListener('click', () => {
     localStorage.clear;
 
-    window.location.href('../');
+    window.location.replace('./');
 })
